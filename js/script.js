0b2864d5ab5217e14a7f7f2bf61e87a4d36cc963
AYKdgreat <dalemoayokunlephilip@gmail.com>
2022-05-12 03:09:06 +0100

    First commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 eruda.init()
+
+let pageContent = document.querySelector("#page-content")
+let loading = document.querySelector("#loading")
+
+function removeLoader () {
+  let remove = setTimeout(function showPageContent() {
+    loading.style.display = "none"
+    pageContent.style.display = "block"
+  }, 2500);
+}
+
 function openNav() {
   document.querySelector('#nav-body').style.width = '100%'
   document.querySelector('.hamburger').style.visibility = 'hidden'
@@ -19,10 +30,37 @@ let details = [senderName, senderEmail, messageSubject, messageBody]
 
 let contactForm = document.forms[0];
 sendBtn.addEventListener('click', function validate(e) {
+e.preventDefault()
   for (i = 0; i < details.length; i++) {
     if (details[i].value !== "") {
       details[i].style.borderColor = "teal"
-      console.log(details[i].value)
+        const formData = new FormData()
+        //const formData = [{'name': details[0].value}, {'email':details[1].value},{'subject':details[2].value},{'body':details[3].value}];
+        formData.append(
+           'name',
+           details[0].value
+       )
+        formData.append(
+           'email',
+           details[1].value
+       )
+        formData.append(
+           'subject',
+           details[2].value
+       )
+        formData.append(
+           'body',
+           details[3].value
+       )
+        
+        fetch("https://getform.io/f/6bc58a62-b63d-4ba6-812a-e77c55beebe4",
+          {
+            method: "POST",
+            body: formData,
+          })
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
+      
     } else {
       details[i].style.borderColor = "red"
       details[i].placeholder = "Required field";
